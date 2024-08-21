2c91abcb07303127a9e41181a01a68c2e879cea7
duongthang123 <88389413+duongthang123@users.noreply.github.com>
2024-08-21 11:23:12 +0700

    fix bug update scores student

--- public/admin/assets/js/update-student-score.js
+++ public/admin/assets/js/update-student-score.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     const studentSubjects = window.studentSubjects;
     const subjects = window.subjects.data;
 
-    const $scoreUpdateBodyTable = $('#scores-table-body');
-    const $btnCreateNewRow = $('#create-row-update-score');
-    const $btnSubmitUpdate = $('#btn-submit-update-score');
+    const scoreUpdateBodyTable = $('#scores-table-body');
+    const btnCreateNewRow = $('#create-row-update-score');
+    const btnSubmitUpdate = $('#btn-submit-update-score');
 
     // get selected values
     function getSelectedValues() {
@@ -17,52 +17,92 @@ $(document).ready(function() {
         });
 
         if (selectedValues.length > 0) {
-            $btnSubmitUpdate.show();
+            btnSubmitUpdate.show();
         } else {
-            $btnSubmitUpdate.hide();
+            btnSubmitUpdate.hide();
         }
 
         return selectedValues;
     }
 
     // Update options in selected form
+    // function updateOptions() {
+    //     const selectedValues = getSelectedValues();
+        
+    //     $('select').each(function() {
+    //         const select = $(this);
+    //         let options = '<option value="">Select Subject</option>';
+
+    //         subjects.forEach(subject => {
+    //             if (!selectedValues.includes(subject.id.toString()) || select.val() === subject.id.toString()) {
+    //                 const registeredSubject = studentSubjects.find(stuSubject => stuSubject.id == subject.id);
+    //                 const score = registeredSubject ? registeredSubject.pivot.score : '';
+    //                 options += `<option value="${subject.id}" data-score="${score !== null ? score : ''}" ${select.val() === subject.id.toString() ? 'selected' : ''}>${subject.name}</option>`;
+    //             }
+    //         });
+
+    //         select.html(options);
+    //     });
+
+    //     $('select').each(function() {
+    //         const select = $(this);
+    //         const selectedSubjectId = select.val();
+    //         const inputElement = select.closest('tr').find('input');
+        
+    //         if (!selectedSubjectId) {
+    //             inputElement.val('');
+    //         } else {
+    //             const registeredSubject = studentSubjects.find(subject => subject.id == selectedSubjectId);
+    //             inputElement.attr('name', `scores[${selectedSubjectId}]`);
+    //             if (registeredSubject && registeredSubject.pivot) {
+    //                 const score = registeredSubject.pivot.score !== undefined ? registeredSubject.pivot.score : '';
+    //                 inputElement.val(score);
+    //             } else {
+    //                 inputElement.val(''); 
+    //             }
+    //         }
+    //     });
+        
+    //     toggleCreateBtn();
+    // }
+
     function updateOptions() {
         const selectedValues = getSelectedValues();
+    
         $('select').each(function() {
-            const $select = $(this);
+            const select = $(this);
+            const selectedSubjectId = select.val();
             let options = '<option value="">Select Subject</option>';
-
+    
             subjects.forEach(subject => {
-                if (!selectedValues.includes(subject.id.toString()) || $select.val() === subject.id.toString()) {
+                const isSelected = selectedSubjectId === subject.id.toString();
+                const isAvailable = !selectedValues.includes(subject.id.toString()) || isSelected;
+                if (isAvailable) {
                     const registeredSubject = studentSubjects.find(stuSubject => stuSubject.id == subject.id);
                     const score = registeredSubject ? registeredSubject.pivot.score : '';
-                    options += `<option value="${subject.id}" data-score="${score || ''}" ${$select.val() === subject.id.toString() ? 'selected' : ''}>${subject.name}</option>`;
+                    options += `<option value="${subject.id}" data-score="${score !== null ? score : ''}" ${isSelected ? 'selected' : ''}>${subject.name}</option>`;
                 }
             });
-
-            $select.html(options);
-        });
-
-        $('select').each(function() {
-            const $select = $(this);
-            const selectedSubjectId = $select.val();
-            const $inputElement = $select.closest('tr').find('input');
+    
+            select.html(options);
+    
+            const inputElement = select.closest('tr').find('input');
             if (!selectedSubjectId) {
-                $inputElement.val('');
-            } else if (!$inputElement.attr('name') || $inputElement.attr('name') !== `scores[${selectedSubjectId}]`) {
+                inputElement.val('');
+            } else {
                 const registeredSubject = studentSubjects.find(subject => subject.id == selectedSubjectId);
-                if (registeredSubject) {
-                    $inputElement.val(registeredSubject.pivot.score);
-                }
+                const score = registeredSubject && registeredSubject.pivot ? registeredSubject.pivot.score : '';
+                inputElement.attr('name', `scores[${selectedSubjectId}]`).val(score);
             }
         });
+    
         toggleCreateBtn();
     }
-
+    
     // create new row update
     function createNewRow() {
         const selectedValues = getSelectedValues();
-        const $newRow = $('<tr></tr>');
+        const newRow = $('<tr></tr>');
         let options = '<option value="">Select Subject</option>';
 
         subjects.forEach(subject => {
@@ -73,7 +113,7 @@ $(document).ready(function() {
             }
         });
 
-        $newRow.html(`
+        newRow.html(`
             <td>
                 <div class="form-group">
                     <select class="custom-select subject-select">
@@ -95,37 +135,37 @@ $(document).ready(function() {
             </td>
         `);
 
-        const $selectElement = $newRow.find('select');
-        const $inputElement = $newRow.find('input');
-        const $removeButton = $newRow.find('.btn-remove');
+        const selectElement = newRow.find('select');
+        const inputElement = newRow.find('input');
+        const removeButton = newRow.find('.btn-remove');
 
-        $selectElement.on('change', function() {
-            const selectedSubjectId = $selectElement.val();
-            const selectedOption = $selectElement.find('option:selected');
+        selectElement.on('change', function() {
+            const selectedSubjectId = selectElement.val();
+            const selectedOption = selectElement.find('option:selected');
             const score = selectedOption.data('score');
             
-            $inputElement.attr('name', `scores[${selectedSubjectId}]`);
-            $inputElement.val(score || '');
-            updateOptions();
+            inputElement.attr('name', `scores[${selectedSubjectId}]`);
+            inputElement.val(score || '');
+            // updateOptions();
         });
 
-        $removeButton.on('click', function() {
-            $newRow.remove();
+        removeButton.on('click', function() {
+            newRow.remove();
             updateOptions();
             toggleCreateBtn();
         });
 
-        $scoreUpdateBodyTable.append($newRow);
+        scoreUpdateBodyTable.append(newRow);
     }
 
     // change option in row
-    $scoreUpdateBodyTable.on('change', '.subject-select', function() {
+    scoreUpdateBodyTable.on('change', '.subject-select', function() {
         updateOptions();
         toggleCreateBtn();
     });
 
     // delete row select
-    $scoreUpdateBodyTable.on('click', '.btn-remove', function() {
+    scoreUpdateBodyTable.on('click', '.btn-remove', function() {
         $(this).closest('tr').remove();
         updateOptions();
         toggleCreateBtn();
@@ -135,9 +175,9 @@ $(document).ready(function() {
     function toggleCreateBtn() {
         const selectedValues = getSelectedValues();
         if (selectedValues.length === subjects.length) {
-            $btnCreateNewRow.hide();
+            btnCreateNewRow.hide();
         } else {
-            $btnCreateNewRow.show();
+            btnCreateNewRow.show();
         }
     }
 
@@ -145,29 +185,29 @@ $(document).ready(function() {
     function validateForm() {
         let isValid = true;
         $('select').each(function() {
-            const $select = $(this);
-            const $feedbackElement = $select.next('span.text-danger');
-            if ($select.val() === '') {
+            const select = $(this);
+            const feedbackElement = select.next('span.text-danger');
+            if (select.val() === '') {
                 isValid = false;
-                $select.addClass('is-invalid');
-                $feedbackElement.text('Please select a subject.');
-                $feedbackElement.show();
+                select.addClass('is-invalid');
+                feedbackElement.text('Please select a subject.');
+                feedbackElement.show();
             } else {
-                $select.removeClass('is-invalid');
-                $feedbackElement.hide();
+                select.removeClass('is-invalid');
+                feedbackElement.hide();
             }
         });
         return isValid;
     }
 
-    $btnSubmitUpdate.on('click', function(event) {
+    btnSubmitUpdate.on('click', function(event) {
         if (!validateForm()) {
             event.preventDefault();
         }
     });
 
     // Create new row
-    $btnCreateNewRow.on('click', function() {
+    btnCreateNewRow.on('click', function() {
         createNewRow();
     });
 
